08d7310cf8617a8cd5a1a38fa9cc30aa11ae2843
CodingPost <99516396+CodingPost-0@users.noreply.github.com>
2022-02-28 07:34:37 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let menu = document.querySelector('#menu-btn');
+let navbarLinks = document.querySelector('.header .navbar .links');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbarLinks.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbarLinks.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .navbar').classList.add('active');
+   }else{
+      document.querySelector('.header .navbar').classList.remove('active');
+   }
+};
+
+var swiper = new swiper(".home-slider", {
+   loop:true,
+   grabCursor:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+   breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
